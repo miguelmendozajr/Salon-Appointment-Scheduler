c6b56e5f4d20361e5b3a22420051d7186ab21d76
miguelmendozajr <117315115+miguelmendozajr@users.noreply.github.com>
2023-02-08 22:24:06 -0600

    commit

---
+++ salon.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+echo -e "\n~~~~~ MY SALON ~~~~~\n"
+
+echo -e "Welcome to My Salon, how can I help you?\n"
+
+PSQL="psql -X --username=freecodecamp --dbname=salon --tuples-only -c"
+
+MAIN_MENU() {
+
+  if [[ $1 ]]
+  then
+   echo -e "\n$1"
+  fi
+
+  SERVICES=$($PSQL "SELECT service_id, name FROM services ORDER BY service_id")
+  
+  echo "$SERVICES" | while read SERVICE_ID BAR SERVICE_NAME
+  do
+    echo "$SERVICE_ID) $SERVICE_NAME"
+  done
+
+  read SERVICE_ID_SELECTED
+  if [[ ! $SERVICE_ID_SELECTED =~ ^[0-9]+$ ]]
+   then
+     MAIN_MENU "I could not find that service. What would you like today?"
+   else
+    SERVICE_EXISTANCE=$($PSQL "SELECT service_id FROM services WHERE service_id=$SERVICE_ID_SELECTED")
+    if [[ -z $SERVICE_EXISTANCE ]]
+    then
+      MAIN_MENU "I could not find that service. What would you like today?"
+    else
+
+      SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id=$SERVICE_ID_SELECTED")
+
+      echo -e "\nWhat's your phone number?"
+      read CUSTOMER_PHONE
+
+      CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
+      CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone='$CUSTOMER_PHONE'")
+
+      if [[ -z $CUSTOMER_NAME ]]
+      then        
+        echo -e "\nI don't have a record for that phone number, what's your name?"
+        read CUSTOMER_NAME
+        NEW_CUSTOMER=$($PSQL "INSERT INTO customers(name, phone) VALUES('$CUSTOMER_NAME', '$CUSTOMER_PHONE')")
+        CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
+      fi
+
+      echo -e "\nWhat time would you like your$SERVICE_NAME, $CUSTOMER_NAME?"
+      read SERVICE_TIME
+
+      NEW_APPOINTMENT=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
+
+      echo -e "\nI have put you down for a$SERVICE_NAME at $SERVICE_TIME, $CUSTOMER_NAME."
+
+    fi
+  fi
+
+}
+
+MAIN_MENU
